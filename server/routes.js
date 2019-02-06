3567bba47385316957c16f654bd708d4ceb5aea9
gbarrbrown <gbb7899@gmail.com>
2019-02-06 18:31:41 +1300

    make a route work with :id

--- server/routes.js
+++ server/routes.js
@@ -2,11 +2,16 @@ const express = require('express')
 
 const router = express.Router()
 
+const db = require('./db/db')
+
 router.use(express.json())
 
 //set routes in here
 
-// router.get('/' ,(req, res) => {
-// })
+router.get('/:id' ,(req, res) => {
+    console.log(req.params.id)
+    const id = JSON.stringify(req.params.id)
+    res.json(id)
+})
 
 module.exports = router
--- server/server.js
+++ server/server.js
@@ -1,17 +1,18 @@
 const path = require('path')
 const express = require('express')
-const routes = require('./routes')
 
 const server = express()
 
+const dbRoutes = require('./routes')
+
 server.use(express.json())
 server.use(express.static(path.join(__dirname, './public')))
-server.use('/v1/routes', routes)
+server.use('/v1/routes', dbRoutes)
 
 
 // unsure if i need this here?? 
 server.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public'))
+    res.sendFile(path.join(__dirname, './public'))
 })
 
 module.exports = server
