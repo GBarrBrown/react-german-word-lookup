b66fe33a4be8712a1a10bbec7b8375f8a99bbaf6
gbarrbrown <gbb7899@gmail.com>
2019-02-12 18:33:48 +1300

    results containers get classNames

--- client/components/Results.jsx
+++ client/components/Results.jsx
@@ -3,13 +3,15 @@ import React from 'react'
 const Results = (props) => {
     var searchResultsArr = props.searchResults
     return(
-        <div>
-            <ul>
-    {searchResultsArr.map((result, key) => {
-        return<li key={key}>{result.word}</li>
-        }
-        )}
-            </ul>
+        <div className="outer-results-container">
+            <div className="results-container">
+                <ul>
+        {searchResultsArr.map((result, key) => {
+            return<li key={key}>{result.word}</li>
+            }
+            )}
+                </ul>
+            </div>
         </div>
     )
 }
