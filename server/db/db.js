9a0e194c648398c3b810b44423e0be21fafc6f70
gbarrbrown <gbb7899@gmail.com>
2019-05-07 16:07:26 +1200

    return correct data, not waiting for all promises to resolve

--- server/db/db.js
+++ server/db/db.js
@@ -2,11 +2,25 @@ const config = require('../../knexfile').development
 const connection = require('knex')(config)
 
 module.exports = {
-    searchWord
+    searchWord,
+    getTranslations,
+    getEnglishById
 }
 
 function searchWord(searchStr, db = connection) {
     return db('german_words')
     .where('word', 'like', ''+searchStr+'%')
+    .select()
+}
+
+function getTranslations(gWordId, db = connection) {
+    return db('translation_lookup')
+    .where('german_id', gWordId)
+    .select()
+}
+
+function getEnglishById(id, db = connection) {
+    return db('english_words')
+    .where('id', id)
     .select()
 }
