b7331ee47137366d27cfc6fdb38cd2f74605cd0e
gbarrbrown <gbb7899@gmail.com>
2019-05-11 20:25:52 +1200

    display translations

--- client/components/Results.jsx
+++ client/components/Results.jsx
@@ -6,10 +6,17 @@ const Results = (props) => {
         <div className="outer-results-container">
             <div className="results-container">
                 <ul>
-        {searchResultsArr.map((result, key) => {
-            return<li key={key}>{result.word}</li>
-            }
-            )}
+                    {searchResultsArr.map((result, key) => {
+                        return<li key={key}>
+                        <strong>{result.word}</strong> - {result.translations.map((translation, i) => {
+                            if(i == 0){
+                                return <span>{translation}</span>
+                            }else{
+                                return <span>, {translation}</span>
+                            }
+                        })}
+                            </li>
+                    })}
                 </ul>
             </div>
         </div>
